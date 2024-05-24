919098a5e2404188730eb80d8972d7ed277a43bb
55 ОМ <230895@astanait.edu.kz>
2024-05-24 08:58:14 +0600

    2 commit

--- main.py
+++ main.py
@@ -4,35 +4,52 @@ class Graph:
     def __init__(self):
         self.adjacency_list = defaultdict(list)
 
-    def add_edge(self, u, v):
-        self.adjacency_list[u].append(v)
+    def add_vertex(self, vertex):
+        if vertex not in self.adjacency_list:
+            self.adjacency_list[vertex] = []
 
-    def dfs(self, start):
+    def add_edge(self, source, destination):
+        self.adjacency_list[source].append(destination)
+
+    def depth_first_search(self, source):
         visited = set()
-        stack = [start]
+        self.dfs_util(source, visited)
 
-        while stack:
-            vertex = stack.pop()
-            if vertex not in visited:
-                print(vertex, end=' ')
-                visited.add(vertex)
-                stack.extend([neighbor for neighbor in self.adjacency_list[vertex] if neighbor not in visited])
+    def dfs_util(self, vertex, visited):
+        visited.add(vertex)
+        print(vertex, end=" ")
 
-    def bfs(self, start):
+        for neighbor in self.adjacency_list[vertex]:
+            if neighbor not in visited:
+                self.dfs_util(neighbor, visited)
+
+    def breadth_first_search(self, source):
         visited = set()
-        queue = deque([start])
+        queue = deque()
+        queue.append(source)
+        visited.add(source)
 
         while queue:
             vertex = queue.popleft()
-            if vertex not in visited:
-                print(vertex, end=' ')
-                visited.add(vertex)
-                queue.extend([neighbor for neighbor in self.adjacency_list[vertex] if neighbor not in visited])
+            print(vertex, end=" ")
 
-# Create graph
+            for neighbor in self.adjacency_list[vertex]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.append(neighbor)
+
+# Create a graph instance
 graph = Graph()
 
-# Add edges
+# Add vertices and edges to the graph
+graph.add_vertex('A')
+graph.add_vertex('B')
+graph.add_vertex('C')
+graph.add_vertex('D')
+graph.add_vertex('E')
+graph.add_vertex('F')
+graph.add_vertex('G')
+
 graph.add_edge('A', 'C')
 graph.add_edge('A', 'B')
 graph.add_edge('A', 'D')
@@ -44,8 +61,11 @@ graph.add_edge('E', 'G')
 graph.add_edge('E', 'F')
 graph.add_edge('F', 'G')
 
-print("DFS:", end=' ')
-graph.dfs('A')
+# Perform Depth First Search (DFS)
+print("DFS:", end=" ")
+graph.depth_first_search('A')
+print()
 
-print("\nBFS:", end=' ')
-graph.bfs('A')
+# Perform Breadth First Search (BFS)
+print("BFS:", end=" ")
+graph.breadth_first_search('A')
